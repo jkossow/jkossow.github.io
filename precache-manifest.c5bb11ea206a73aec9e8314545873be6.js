2ea04df0db1d20b31cbc186a2d7ad1e76aea3393
Janusz Kossowski <jkossow68@gmail.com>
2018-10-16 14:36:17 +0200

    Updates

---
+++ precache-manifest.c5bb11ea206a73aec9e8314545873be6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "112c292918eef02cc992",
+    "url": "/static/js/main.112c2929.chunk.js"
+  },
+  {
+    "revision": "4fd91c74e51506835aa9",
+    "url": "/static/js/1.4fd91c74.chunk.js"
+  },
+  {
+    "revision": "112c292918eef02cc992",
+    "url": "/static/css/main.b5b6d7d2.chunk.css"
+  },
+  {
+    "revision": "9acc18ecc08873b523d5b6cc92bf796a",
+    "url": "/index.html"
+  }
+];
